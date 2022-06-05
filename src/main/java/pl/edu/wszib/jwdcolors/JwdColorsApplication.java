992d58d59747842adf279c8e186b4acdc22527cd
Piotrek <pgiercza@gmail.com>
2022-06-05 20:38:59 +0200

    Working local

--- src/main/java/pl/edu/wszib/jwdcolors/JwdColorsApplication.java
+++ src/main/java/pl/edu/wszib/jwdcolors/JwdColorsApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@PropertySource("classpath:db.properties")
+//@PropertySource("classpath:db.properties")
 public class JwdColorsApplication {
 
     public static void main(String[] args) {
